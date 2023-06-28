1d028c81a66fbaa87e7411cf107bf91fcbe7be3b
Cloakd <thecloakdone@gmail.com>
2023-06-28 11:16:36 +0100

    Add pagination to collections index

--- src/api/getCollections.ts
+++ src/api/getCollections.ts
@@ -1,7 +1,7 @@
 import {getApi} from "./core";
 import {CollectionResponse} from "../entities";
 
-export async function getCollections() {
-	const resp = await getApi("collections")
+export async function getCollections(page = 0, limit = 500) {
+	const resp = await getApi(`collections?page=${page}&limit=${limit}`)
 	return resp as CollectionResponse
 }
