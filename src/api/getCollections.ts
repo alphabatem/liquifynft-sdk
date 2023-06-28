9a7293d2fdf219f966d77240a9eb521c3bb3cfcf
Cloakd <thecloakdone@gmail.com>
2023-06-28 12:19:43 +0100

    Add search to collections

--- src/api/getCollections.ts
+++ src/api/getCollections.ts
@@ -1,7 +1,7 @@
 import {getApi} from "./core";
 import {CollectionResponse} from "../entities";
 
-export async function getCollections(page = 0, limit = 500) {
-	const resp = await getApi(`collections?page=${page}&limit=${limit}`)
+export async function getCollections(page = 0, limit = 500,search="") {
+	const resp = await getApi(`collections?page=${page}&limit=${limit}&search=${search}`)
 	return resp as CollectionResponse
 }
