a7ea148a2d3c93a33187b4fccf23a2538bfe9661
Cloakd <thecloakdone@gmail.com>
2023-06-29 12:11:17 +0100

    Add onchain to collections

--- src/api/getCollections.ts
+++ src/api/getCollections.ts
@@ -1,7 +1,13 @@
 import {getApi} from "./core";
 import {CollectionResponse} from "../entities";
 
-export async function getCollections(page = 0, limit = 500,search="") {
-	const resp = await getApi(`collections?page=${page}&limit=${limit}&search=${search}`)
+export async function getCollections(page = 0, limit = 500,search="", onChainOnly = false) {
+	let uri = `collections?page=${page}&limit=${limit}`
+	if (search !== "")
+		uri = `${uri}&search=${search}`
+	if (onChainOnly)
+		uri = `${uri}&onchain=true`
+
+	const resp = await getApi(uri)
 	return resp as CollectionResponse
 }
